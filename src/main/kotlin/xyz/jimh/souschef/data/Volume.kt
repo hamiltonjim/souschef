1512a85c3897cde1233bda77db9f04a9f4eb00fe
Jim Hamilton <jhamilton@meteorcomm.com>
2025-03-13 11:51:29 -0700

    Added constructors from the type AUnit

--- src/main/kotlin/xyz/jimh/souschef/data/Volume.kt
+++ src/main/kotlin/xyz/jimh/souschef/data/Volume.kt
@@ -20,4 +20,31 @@ class Volume(
     var intl: Boolean,
     var abbrev: String? = null,
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null
-)
+) {
+    constructor(unit: AUnit) : this(unit.name, unit.inBase, unit.intl, unit.abbrev, unit.id)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Volume
+
+        if (name != other.name) return false
+        if (inMl != other.inMl) return false
+        if (intl != other.intl) return false
+        if (abbrev != other.abbrev) return false
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + inMl.hashCode()
+        result = 31 * result + intl.hashCode()
+        result = 31 * result + (abbrev?.hashCode() ?: 0)
+        result = 31 * result + (id?.hashCode() ?: 0)
+        return result
+    }
+
+}
--- src/main/kotlin/xyz/jimh/souschef/data/Weight.kt
+++ src/main/kotlin/xyz/jimh/souschef/data/Weight.kt
@@ -20,4 +20,30 @@ class Weight(
     var intl: Boolean,
     var abbrev: String? = null,
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null
-)
+) {
+    constructor(unit: AUnit) : this(unit.name, unit.inBase, unit.intl, unit.abbrev, unit.id)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Weight
+
+        if (name != other.name) return false
+        if (inGrams != other.inGrams) return false
+        if (intl != other.intl) return false
+        if (abbrev != other.abbrev) return false
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + inGrams.hashCode()
+        result = 31 * result + intl.hashCode()
+        result = 31 * result + (abbrev?.hashCode() ?: 0)
+        result = 31 * result + (id?.hashCode() ?: 0)
+        return result
+    }
+}
